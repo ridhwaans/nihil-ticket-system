1fea6c155c2f38ad51f7a87c5b7c35e350e2cd1f
Wes Unwin <wes_unwin@hotmail.com>
2013-03-05 17:30:41 -0500

    testTicket Complete

--- tests/testTicket.cpp
+++ tests/testTicket.cpp
@@ -8,15 +8,16 @@ using namespace std;
 
 int main ( int argc, char** argv, char** envp) {
 
-	char inputstring[] = "eeeeeeeeeeeeeeevent uuuuuuuusername 100 100.00";
+	char inputstring[] = "Theeeeeeeeeeeeevent Auuuuuuusername 100 100.00";
 
 	Ticket* t = new Ticket( (char*) inputstring );
 	
+	
    cout << "Testing Ticket.cpp..." << endl;
    cout << "Event Name: "          << t->eventName << endl;
-	cout << "Username: "            << t->username << endl;
-	cout << "Quantity: "            << t->quantity << endl;
-   cout << "Price: "               << t->price << endl;
+	cout << "Username: "            << t->username  << endl;
+	cout << "Quantity: "            << t->quantity  << endl;
+   cout << "Price: "               << t->price     << endl;
 
 	cout << "Done.";
 
