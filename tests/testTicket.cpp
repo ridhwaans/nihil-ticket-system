0a0e28f7a0ef3d6bda0ba6f590bafe66a2f7a054
Wes Unwin <wes_unwin@hotmail.com>
2013-03-05 15:27:41 -0500

    testTicket.cpp created

---
+++ tests/testTicket.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <string.h>
+#include "../assets/globals.h"
+#include "../assets/Ticket.h"
+
+int main ( int argc, char** argv, char** envp) {
+	error_string = new char[error_size];
+
+	char* inputstring = "eeeeeeeeeeeeeeeeeee uuuuuuuuuuuuuuu 100 100.00";
+
+	Ticket* t = new Ticket(inputstring);
+
+	
+	printf( "Done Testing Ticket" );
+}
