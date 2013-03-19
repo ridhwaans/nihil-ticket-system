9ae2a61aa75a12dd6535ee83d4c164aea986c134
Wes Unwin <wes_unwin@hotmail.com>
2013-03-19 15:17:51 -0400

    debug_tickets set back to false

--- assets/debug.h
+++ assets/debug.h
@@ -1,11 +1,11 @@
 #ifndef asssets_debug_h
 #define asssets_debug_h
 
-#define debug_accounts false
-#define debug_tickets true
-#define debug_transactions false
+#define debug_accounts			false
+#define debug_tickets 			false
+#define debug_transactions 	false
 
-#define promptEnabled true
-#define buy_ticketList false
+#define promptEnabled 			true
+#define buy_ticketList 			false
 
 #endif
