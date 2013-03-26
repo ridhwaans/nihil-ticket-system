dc7567240b4387c462f675fe662ef080b7324f5b
Wes Unwin <wes_unwin@hotmail.com>
2013-03-26 17:12:14 -0400

    Ticket.java started...

---
+++ jsrc/assets/Ticket.java
@@ -0,0 +1,26 @@
+
+public class Ticket
+{
+
+	/**
+	 * Constructs a new Ticket object, from the given line of text
+    * from the available tickets file.
+    */
+	public Ticket(String TicketsFileLine) {
+
+
+	}
+
+
+	/**
+    *  Returns the string representation of this ticket, in a format
+    *  suitable to be written to an available tickets file.
+    */
+	public String toString() {
+
+
+	}
+
+
+
+}
