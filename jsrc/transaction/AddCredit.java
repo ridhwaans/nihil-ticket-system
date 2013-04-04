43d535abc4a972b2eae2ad7d92ca5bad737c3ed1
BrittusNightmare <superflash54@gmail.com>
2013-04-03 20:20:47 -0400

    Updated AddCredit and Refund

--- jsrc/transaction/AddCredit.java
+++ jsrc/transaction/AddCredit.java
@@ -2,18 +2,33 @@ package transaction;
 
 //library imports
 import java.util.Vector;
-import java.util.zip.DataFormatException;
+import java.io.*;
 //local imports
 import assets.*;
 
-/** @class AddCredit
- * @brief Represents an 'addcredit' transaction.
- **/
-public class AddCredit extends Transaction {
+public class AddCredit extends Transaction{
 	public static final int code = 6;
-	public AddCredit( String s) throws DataFormatException {
-		System.out.println(s);}
-	public void applyTo (
-		Vector<Account> accounts, Vector<Ticket> tickets)
-			throws TransactionException{}
+	String username;
+	int credit;
+	public AddCredit( String s){
+		FileInputStream fstream = new FileInputStream("*.dtf");
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		while ((s = br.readLine()) != null){
+
+			username = s.substring(3, 15);
+			username.replaceAll(" ","");
+			credit = Integer.parseInt(s.substring(21, 9));
+		}
+	
+		System.out.println(s);
+	}
+	public void applyTo (Vector<Account> accounts, Vector<Ticket> tickets)throws TransactionException{
+		FileWriter fstream = new FileWriter("data.cua");
+		BufferedWriter out = new BufferedWriter(fstream);
+		out.write(username);
+		out.write(credit);
+		out.close();
+	}
 }
+
--- jsrc/transaction/Refund.java
+++ jsrc/transaction/Refund.java
@@ -1,6 +1,12 @@
 package transaction;
 
 //library imports
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
 import java.util.Vector;
 import java.util.zip.DataFormatException;
 //local imports
@@ -11,9 +17,31 @@ import assets.*;
  **/
 public class Refund extends Transaction {
 	public static final int code = 5;
+	String buyerUsername;
+	String sellerUsername;
+	int refund;
 	public Refund( String s) throws DataFormatException {
+		FileInputStream fstream = new FileInputStream("*.dtf");
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader buf = new BufferedReader(new InputStreamReader(in));
+		while ((s = buf.readLine()) != null){
+
+			buyerUsername = s.substring(3, 15);
+			buyerUsername.replaceAll(" ","");
+			sellerUsername = s.substring(18, 15);
+			sellerUsername.replaceAll(" ","");
+			refund = Integer.parseInt(s.substring(35, 9));
+
+		}
 		System.out.println(s);}
-	public void applyTo (
-		Vector<Account> accounts, Vector<Ticket> tickets)
-			throws TransactionException{}
+	public void applyTo (Vector<Account> accounts, Vector<Ticket> tickets)throws TransactionException{
+		FileWriter fstream = new FileWriter("data.cua");
+		BufferedWriter out = new BufferedWriter(fstream);
+		out.write(buyerUsername);
+		out.write(refund);
+		out.write(sellerUsername);
+		out.write(refund);
+		out.close();
+	}
+
 }
