a4f84cfc2d549f6c45177ea08c7807332989f3e5
ridhwaans <ridhwaans@yahoo.com>
2013-04-03 22:56:59 -0300

    create and delete

--- jsrc/transaction/Delete.java
+++ jsrc/transaction/Delete.java
@@ -6,51 +6,48 @@ import java.util.zip.DataFormatException;
 //local imports
 import assets.*;
 
-/** @class Delete
- * @brief Represents a 'delete' transaction.
+/** @class Delete class represents a 'delete' transaction.
  **/
 public class Delete extends Transaction {
 	public static final int code = 2;
+	
+	public static final int username_size  = 15;
+	public static final int credit_size = 6;
+	public static final int usertype_size = 2;
+	
+	//Delete Transaction Parameters
+	public String username; //username of new account to be deleted
+	public int type; //usertype for the specified user to be deleted
+	public int credit; //initial account balance for the user account to be deleted
+	
 	public String deleteTransactionLine;
 	
 	public Delete( String s) throws DataFormatException {
-		System.out.println(s);
-		deleteTransactionLine = s;}
+		//read and extract fields from the transaction string
+		this.username = s.substring(0,username_size-1).trim();
+				
+		if(	s.substring(username_size+1,username_size+1+usertype_size-1).toUpperCase().equals("AA") )
+			this.type = 0;
+		else if( s.substring(username_size+1,username_size+1+usertype_size-1).toUpperCase().equals("BS") )
+			this.type = 1;
+		else if( s.substring(username_size+1,username_size+1+usertype_size-1).toUpperCase().equals("SS") )
+			this.type = 2;
+		else if( s.substring(username_size+1,username_size+1+usertype_size-1).toUpperCase().equals("FS") )
+			this.type = 3;
+				
+		//extract integer portion of ticket price (in hundreds of cents)
+		this.credit = 100* Integer.parseInt( s.substring( username_size + 1 + usertype_size + 1, username_size + 1 + usertype_size + 1 + 2 ) );
+
+		//now extract decimal portion adding it to ticket price (# of cents)
+		this.credit += Integer.parseInt( s.substring( username_size + 1 + usertype_size + 1, username_size + 1 + usertype_size + 1 + 2 ) );
+		System.out.println(s);		
+	
+	}
 	
 	public void applyTo (Vector<Account> accounts, Vector<Ticket> tickets) throws TransactionException{
-		boolean validLine= true; //Use boolean flag to determine if all fields in input string parameter line are valid
-		
-		String username;
-		int type=0;
-		int credit=0;
-		
-		int CurIndex = 0;
-		username =  deleteTransactionLine.substring(CurIndex, CurIndex + username_size -1);
-		CurIndex += username_size + 1;
-		
-		if(	deleteTransactionLine.substring(CurIndex, CurIndex + 2).toLowerCase().equals("AA") )
-			type = 0;
-		else if( deleteTransactionLine.substring(CurIndex, CurIndex + 2).toLowerCase().equals("BS") )
-			type = 1;
-		else if( deleteTransactionLine.substring(CurIndex, CurIndex + 2).toLowerCase().equals("SS") )
-			type = 2;
-		else if( deleteTransactionLine.substring(CurIndex, CurIndex + 2).toLowerCase().equals("FS") )
-			type = 3;
-		else
-			validLine = false;
-		
-		CurIndex += 2 + 1;
-		
-		if ((isInteger(deleteTransactionLine.substring(CurIndex, CurIndex + 2))==true) && (isInteger(deleteTransactionLine.substring(CurIndex+4, CurIndex+4+1))==true))
-			credit = Integer.parseInt(deleteTransactionLine.substring(CurIndex, CurIndex + 2))*100  +  Integer.parseInt(deleteTransactionLine.substring(CurIndex+4, CurIndex+4+1)); 
-		else
-			validLine = false;
-	    
-		if (validLine == true){ //if true, apply the transaction
-			Account a = new Account(username, type, credit);
-			accounts.add(a);
-			if (accounts.indexOf(username) == -1){ //or contains
-				accounts.remove(accounts.indexOf(username));}}}
+			//can alternatively use vector<Account>.contains 
+			if (accounts.indexOf(this.username) != -1){ //if specified username of the account to be deleted is found
+				accounts.remove(accounts.indexOf(this.username));}}
 	
 
 	private boolean isInteger(String s) {
